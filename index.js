7b6a5b7f313247f0492b45f5c8585e85644a8915
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2023-10-20 10:02:04 +0530

    mongo db server is added

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
 import express from "express";
-const app=express()
+const app = express()
+
+import dotennv from 'dotenv';
+dotennv.config();
+
 app.use(express.json())
+import mongoose, { model } from "mongoose";
+import {Schema } from "mongoose";
 
-const students=[]
-
-
-// app.get('/chandani',(req,res)=>{
-//     res.send(`
-//     <h1>chandrajyoti adil</h1>
-//     `)
+const loadmongodbserver=()=>{
+    const responseofURI=mongoose.connect(process.env.MONGODB_URI)
+    if(responseofURI){
+        console.log("mongoose connected sucessfully")
+    }
     
+}
+loadmongodbserver()
+
+const StudentSchema=new Schema({
+    name:String,
+    phone:String,
+    email:String
+})
+
+const Student = mongoose.model('Student', StudentSchema);
+
+
+
+// app.get('/study', (req,res)=>{
+//     const {id}=req.query
+//     let stu=null
+//     if(!students){
+//       return res.json({
+//         "result":false,
+//         "messege":"phone not  included"
+//                       })
+
+//     }
+//     students.map(student=>{
+//         if(student.id==id){
+//             stu=student
+//         }
+//     })
+//     res.json(stu)
 // })
 
-app.get('/study',(req,res)=>{
-    const {id}=req.query
-    let stu=null
-    if(!students){
-      return res.json({
-        "result":false,
-        "messege":"phone not  included"
-                      })
 
-    }
-    students.map(student=>{
-        if(student.id==id){
-            stu=student
-        }
-    })
-    res.json(stu)
-})
-app.post('/study',(req,res)=>{
+app.post('/study',async (req,res)=>{
     const {name,phone,email}=req.body
     if(!name){
        return res.json({
@@ -51,24 +68,43 @@ app.post('/study',(req,res)=>{
     }
 
 
-    const id=Math.floor(Math.random()*10000)
+    // const id=Math.floor(Math.random()*10000)
  
-    const obj={
-        "id":id,
-        "name":name,
-        "phone":phone,
-        "email":email
-    }
-    if(obj){
-        students.push(obj)
-    }
+   const studentinstance=new Student({
+    name:name,
+    phone:phone,
+    email:email  
+   })
+    const saveStudent= await studentinstance.save()
 
 
     res.json({
          "result":true,
-        "students":students,
+        "students":saveStudent,
         "messege":"data  load succesfully"
           })
+})
+
+app.get('/students',async(req,res)=>{
+
+    const getStudent= await Student.find()
+    res.json({
+
+  "result":true,
+        "students":getStudent,
+        "messege":"data  load succesfully"
+    })
+})
+
+app.get('/getbyid', async (req,res)=>{
+    const {_id}=req.query
+    const findOneStudent=await Student.findOne({_id:_id})
+    res.json({
+        "result":true,
+        "students":findOneStudent,
+        "messege":"data  load succesfully"
+
+    })
 })
 
 const port=5000
